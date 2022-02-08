0d336a35445408cfc33c73e28cc453ce491cbb1b
ouchen <mouchen@ensup.eu>
2022-02-09 00:43:18 +0100

    create tabls for request voter and condidate

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,11 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->date('date_of_birth');
+            $table->enum('sex', ['man', 'women']);
+            $table->string('country');
+            $table->string('city');
+            $table->boolean('isconfirmed')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
