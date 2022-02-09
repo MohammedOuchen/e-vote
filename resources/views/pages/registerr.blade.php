78c0bb4a185e8c67df2325b99fb2fd3502e906ef
El Mehdi EL KOUAY <elmelkouay@neosys.fr>
2022-02-09 21:43:29 +0100

    commit before pull

--- resources/views/pages/registerr.blade.php
+++ resources/views/pages/registerr.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.home')
 
+
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
@@ -34,53 +35,106 @@
                 <div class="register-form">
                     <div class="sign-in-htm">
                     <form>
+                    @csrf
                         <div class="form-group">
 
                             <div class="group">
-                            <select class="form-select" aria-label="Default select example" placeholder="Choisissez ...">
+                            <select id="ifYes" class="form-select" aria-label="Default select example" placeholder="Choisissez ..." required>
+                                <option disabled selected value default> -- select an option -- </option>    
                                 <option selected>Voter</option>
                                 <option value="1">Candidater</option>
                             </select>
-                            <small id="choice" class="form-text text-muted">Vous voulez voter ou candidater</small>
+                            <small id="mainChoice" class="form-text text-muted">Vous voulez voter ou candidater</small>
+                            </div>
+
+                            <div class="group">
+                            <select onchange="yesnoCheck(this);" class="form-select" aria-label="Default select example" placeholder="Choisissez ...">
+                                <option selected>Elections municipales</option>
+                                <option value="1">Elections départementales</option>
+                                <option value="2">Elections régionales</option>
+                                <option value="3">Elections législatives</option>
+                            </select>
+                            <small id="electionChoice" class="form-text text-muted">Choisissez l'election où vous voulez candidatez</small>
                             </div>
   
                             <div class="group">
                                 <label for="voterPrenom" class="voterPrenom">Prénom</label>
-                                <input id="voterPrenom" type="text" class="input">
+                                <input id="voterPrenom" type="text" class="form-control @error('name') is-invalid @enderror" name="voterPrenom" value="{{ old('voterPrenom') }}" required autocomplete="name" autofocus>
+
+                                @error('voterPrenom')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
 
                             <div class="group">
-                                <label for="voter" class="voterNom">Nom</label>
-                                <input id="voterNom" type="text" class="input">
+                                <label for="voterNom" class="voterNom">Nom</label>
+                                <input id="voterNom" type="text" class="form-control @error('name') is-invalid @enderror" name="voterNom" value="{{ old('voterNom') }}" required autocomplete="name" autofocus>
+
+                                @error('voterNom')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+
                             </div>
 
                             <div class="group">
                                 <label for="voterCIN" class="voterCIN">N° de carte national</label>
-                                <input id="voterCIN" type="text" class="input">
+                                <input id="voterCIN" type="text" class="form-control @error('voterCIN') is-invalid @enderror" name="voterCIN" value="{{ old('voterCIN') }}" required autocomplete="name" autofocus>
+
+                                @error('voterCIN')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
 
                             <div class="group">
                                 <label for="voterDN" class="voterDN">Date de naissance</label>
-                                <input id="voterDN" type="text" class="input">
+                                <input id="date_of_birth" type="date" class="form-control @error('date_of_birth') is-invalid @enderror" name="date_of_birth" value="{{ old('date_of_birth') }}" required autocomplete="name" autofocus>
+
+                                @error('date_of_birth')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+
                             </div>
 
                             <div class="group">
                                 <label for="votermail" class="voterDN">Email</label>
-                                <input id="email" type="email" class="input">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
 
                                 <div class="group">
                                     <label for="passMP" class="passMP">Mot de passe</label>
-                                    <input id="passMP" type="password" class="input" data-type="password">
+                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+
                                 </div>
 
                                 <div class="group">
                                     <label for="userCMP" class="userCMP">Confirmer Mot de passe</label>
-                                    <input id="userCMP" type="text" class="input">
+                                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                                 </div>
 
                             <div class="group">
-                                <input type="submit" class="button" value="S'INSCRIRE'">
+                                <button type="submit" class="button">
+                                    {{ __('Register') }}
+                                </button>
                             </div>
                         </div>
                     </form>
@@ -90,7 +144,14 @@
     </div>
 </body>
 <script type="text/javascript">
-
+function yesnoCheck(that) {
+    if (that.value == "other") {
+  alert("check");
+        document.getElementById("ifYes").style.display = "block";
+    } else {
+        document.getElementById("ifYes").style.display = "none";
+    }
+}
 </script>
 
 </html>
